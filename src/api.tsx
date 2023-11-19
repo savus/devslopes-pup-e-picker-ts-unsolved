cf1ec374d69d3dfd70a1e9160adf47cc00b8f965
Savus <justin.fuzzygreenstuff@gmail.com>
2023-11-19 06:30:22 -0700

    complete api.ts fetch request methods for dogs

--- src/api.tsx
+++ src/api.tsx
@@ -1,16 +1,32 @@
+import { Dog } from "./types";
+
 export const baseUrl = "http://localhost:3000";
 
 export const Requests = {
   // should return a promise with all dogs in the database
-  getAllDogs: () => {},
+  getAllDogs: () =>
+    fetch(`${baseUrl}/dogs`).then((response) => response.json()),
   // should create a dog in the database from a partial dog object
   // and return a promise with the result
-  postDog: () => {},
+  postDog: (dog: Omit<Dog, "id">) =>
+    fetch(`${baseUrl}/dogs`, {
+      method: "POST",
+      body: JSON.stringify(dog),
+      headers: { "Content=Type": "application/json" },
+    }).then((response) => response.json()),
 
   // should delete a dog from the database
-  deleteDog: () => {},
+  deleteDog: (id: number) =>
+    fetch(`${baseUrl}/dogs/${id}`, {
+      method: "DELETE",
+    }).then((response) => response.json()),
 
-  updateDog: () => {},
+  updateDog: (dogInfo: Omit<Dog, "id" | "name" | "description">, id: number) =>
+    fetch(`${baseUrl}/dogs/${id}`, {
+      method: "PATCH",
+      body: JSON.stringify(dogInfo),
+      headers: { "Content-Type": "application/json" },
+    }).then((response) => response.json()),
 
   // Just a dummy function for use in the playground
   dummyFunction: () => {
--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,9 @@
 // Add your own custom types in here
+
+export type Dog = {
+  name: string;
+  image: string;
+  description: string;
+  isFavorite: boolean;
+  id: number;
+};
